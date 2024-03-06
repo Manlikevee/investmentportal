704f13b02b39e772e13fa2614d02d7912b1b7abf
manlikevee <odahviktor@gmail.com>
2024-03-06 16:00:06 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+
+function showinvestmentslists() {
+    const investmentlistmodal = document.getElementById('myoverlay')
+
+    investmentlistmodal.classList.toggle('showlist')
+
+}
+
+function showpopup(){
+    let status = 'false'
+    const popup = `
+    <div class="loading-over2" style="display: block;">
+    <div class="popup">
+        <p id="close2"></p>
+        <br>
+    <div class="secoact">
+    <div class="logoac">
+    <img src="img/upd.png" alt="" style="height: 100px; width: 100%; object-fit: contain;">
+    </div>
+
+
+    <div class="logomen"> Kindly Update Your Profile </div>
+
+
+    <div class="logobod">
+
+    You have not yet updated your profile. Please take the necessary steps to provide the required information.
+
+    </div>
+    <div class="logobtn">
+
+                <a href="profileupdate.html" type="button"  class="arwbtn"> Proceed </a>
+
+    </div>
+    </div>
+    </div>
+    </div>
+    ` 
+    if (status == 'false'){
+        document.body.innerHTML += popup
+    } else{
+
+    }
+}showpopup()
